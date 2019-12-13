190139b49b4ea6e917cbabcfc33f319f9952e395
Matthias Kurz <m.kurz@irregular.at>
2019-12-13 09:53:03 +0100

    Added generated file and delete it after streaming zip

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -15,6 +15,8 @@ import play.libs.ws.WSClient;
 import play.mvc.*;
 
 import javax.inject.Inject;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -53,12 +55,20 @@ public class HomeController extends Controller {
             )));
         }
 
-        final Source<ByteString, NotUsed> toStream = sources.via(Archive.zip());
-        // Hier könnte man dann etwas machen wenn das ganze erledig ist, z.b. eben Datei löschen:
-//                .mapMaterializedValue(cs -> {
-//                    Files.delete(Paths.get("/die/datei/.pdf"));
-//                    return cs;
-//                });
+        // Jetzt datei generieren:
+        final Path addedFile = Files.write(Paths.get("hello.txt"), "nur ein test".getBytes(StandardCharsets.UTF_8));
+
+        // Generierte Datei zum zip hinzufügen:
+        sources = sources.concat(Source.single(Pair.create(
+                ArchiveMetadata.create("anderer_subfolder/" + "generierte_datei.txt"),
+                toSource(readFileAsByteString(addedFile))
+        )));
+
+        final Source<ByteString, NotUsed> toStream = sources.via(Archive.zip())
+                .mapMaterializedValue(cs -> {
+                    Files.delete(addedFile); // generierte Datei wieder löschen
+                    return cs;
+                });
 
         return ok().chunked(toStream, false, Optional.ofNullable("download.zip"));
 
@@ -68,6 +78,21 @@ public class HomeController extends Controller {
         CompletionStage<IOResult> ioResult = sources.via(Archive.zip()).runWith(fileSink, mat);
         return ioResult.thenApply(ior -> ok("fertig!"));
 */
+    }
+
+
+    private ByteString readFileAsByteString(Path filePath) throws Exception {
+        final Sink<ByteString, CompletionStage<ByteString>> foldSink =
+                Sink.fold(ByteString.emptyByteString(), ByteString::concat);
+
+        return FileIO.fromPath(filePath)
+                .runWith(foldSink, mat)
+                .toCompletableFuture()
+                .get(3, TimeUnit.SECONDS);
+    }
+
+    private Source<ByteString, NotUsed> toSource(ByteString bs) {
+        return Source.single(bs);
     }
 
 }
